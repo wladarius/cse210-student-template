95b6d163937cfe614b344d6b6715c2654dc480d7
Vladarius <logvladarius@gmail.com>
2023-04-22 12:31:20 +0300

    prep1 and prep2 homework

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,44 @@ using System;
 
 class Program
 {
+    private const int V = 60;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+
+        int x = int.Parse(grade);
+        int A = 90;
+        int B = 80;
+        int C = 70;
+        int D = 60;
+        
+
+        if (x >= A)
+        {
+            Console.WriteLine("Your grade is 'A'!");
+        }
+        else if (x >= B)
+        {
+            Console.WriteLine("Your grade is 'B'!");
+        }
+
+        else if (x >= C)
+        {
+
+            Console.WriteLine("Your grade is 'C'!");
+        }
+
+        else if (x >= D)
+        {   
+            Console.WriteLine("Your grade is 'D'!");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is 'F'!");
+        }
+
+
     }
 }
