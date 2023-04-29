2d485fe52779a30fc2bc4af49523528cf62f550e
Vladarius <logvladarius@gmail.com>
2023-04-29 13:04:01 +0300

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,50 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+      static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        string response = "yes";
+
+        while (response == "yes")
+        {
+
+            Random randomGenerator = new Random();
+            int m_number = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+
+
+            while (m_number != guess)
+            {
+
+                
+
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                    
+
+                if (m_number > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (m_number < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else 
+                {
+                    Console.WriteLine("Got it!");
+                }
+
+
+                }
+                    
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        }
+
+    }   
+
+
 }
